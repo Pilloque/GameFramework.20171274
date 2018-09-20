a72cedafcee0c4abd011d65765a7f59ba20bbe58
영호 김 <zxcv6029@gmail.com>
2018-09-20 17:00:00 +0900

    -9월 20일 -PP05.AnimatingSprite

---
+++ GameFramework/PP05.AnimatingSprite/Game.h
@@ -0,0 +1,27 @@
+#ifndef Game_h
+#define Game_h
+#include "SDL.h"
+#include <iostream>
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	bool m_bRunning;
+};
+
+#endif Game_h

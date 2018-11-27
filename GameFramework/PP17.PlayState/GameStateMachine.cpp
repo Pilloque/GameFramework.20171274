3a406e13348480c2691fb1ae3c2c0f8d5b443c09
정종찬 <chck3685@naver.com>
2018-11-27 21:54:19 +0900

    -11월 27일 -PP17.PlayState

---
+++ GameFramework/PP17.PlayState/GameStateMachine.cpp
@@ -0,0 +1,38 @@
+#include "GameStateMachine.h"
+
+void GameStateMachine::ChangeState(GameState *pState)
+{
+    if (pState != NULL)
+    {
+        if (currentState != NULL)
+        {
+            prevState = currentState;
+            currentState->OnExit();
+        }
+        currentState = pState;
+        currentState->OnEnter();
+    }
+}
+
+void GameStateMachine::PushState(GameState *pState)
+{
+    gameStates.push_back(pState);
+    gameStates.back()->OnEnter();
+}
+
+void GameStateMachine::PopState()
+{
+    ChangeState(prevState);
+}
+
+void GameStateMachine::Update()
+{
+    if (currentState != NULL)
+        currentState->Update();
+}
+
+void GameStateMachine::Render()
+{
+    if (currentState != NULL)
+        currentState->Render();
+}
---
+++ GameFramework/PP17.PlayState/PlayState.cpp
@@ -0,0 +1,52 @@
+#include "PlayState.h"
+#include "Game.h"
+#include "TextureManager.h"
+#include "Player.h"
+#include <iostream>
+
+PlayState* PlayState::pInstance = nullptr;
+const std::string PlayState::playID = "PLAY";
+
+void PlayState::Update()
+{
+    for (std::vector<GameObject*>::size_type i = 0; i < gameObjects.size(); i++)
+    {
+        gameObjects[i]->Update();
+    }
+}
+
+void PlayState::Render()
+{
+    for (std::vector<GameObject*>::size_type i = 0; i < gameObjects.size(); i++)
+    {
+        gameObjects[i]->Draw();
+    }
+}
+
+bool PlayState::OnEnter()
+{
+    if (!TextureManager::Instance()->Load("../assets/helicopter.png", "helicopter", Game::Instance()->GetRenderer()))
+    {
+        return false;
+    }
+
+    GameObject* player = new Player(new LoaderParams(100, 100, 128, 55, "helicopter"));
+    gameObjects.push_back(player);
+    std::cout << "entering PlayState\n";
+
+    return true;
+}
+
+bool PlayState::OnExit()
+{
+    for (std::vector<GameObject*>::size_type i = 0; i < gameObjects.size(); i++)
+    {
+        gameObjects[i]->Clean();
+    }
+    gameObjects.clear();
+
+    TextureManager::Instance()->ClearFromTextureMap("helicopter");
+    std::cout << "exiting PlayState\n";
+
+    return true;
+}
